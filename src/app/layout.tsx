72672f9497543964ce3ac80f07db8aaa7e6a2a96
Thanveer <thanveersiddiq@gmail.com>
2025-07-13 20:47:32 +0530

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/components/providers/session-provider";
 import { Suspense } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Suspense>
+          <Analytics />
           <Providers>
             {children}
           </Providers>
